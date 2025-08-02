a04860facb45ac8d80b14db06b041856705f81c8
SHRI VENKATESH BABU B <shrivenkatesh679@gmail.com>
2025-08-02 13:16:47 +0530

    styling Sass and React Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/SignUp/Signup";
 import Home from "./Pages/HomePage/Home";
 import Products from "./Pages/Products/Products";
 import ProductList from "./Pages/Products/ProductList";
 import ProductDetails from "./Pages/Products/ProductDetails";
-import Footer from "./Components/Footer";
+
 const App = () => {
   return (
     <div>
       <Router>
-        <Header />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
@@ -23,7 +21,6 @@ const App = () => {
             <Route path="/products/?:id" element={<ProductDetails />} />
           </Route>
         </Routes>
-        <Footer />
       </Router>
     </div>
   );
--- src/Pages/Products/ProductList.jsx
+++ src/Pages/Products/ProductList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div></div>
+    <div>ProductList</div>
   )
 }
 
